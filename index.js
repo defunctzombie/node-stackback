dff3b52156d8663329b39fc1ea3f21a6ae4a3fd1
Roman Shtylman <shtylman@gmail.com>
2012-10-10 16:43:47 -0400

    capture stackframes without changing stack variable
    Capture the frames alongside what the stack variable would normally be
    set to. A user should be able to call 'err.stack' after capturing a
    stacktrace without some strange variable being returned.

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-var capture = function (err, trace) {
-    return trace;
-}
 
+// v8 builtin format stack trace
+// for when there was no previous prepareStackTrace function to call
+var FormatStackTrace = require('./formatstack');
+
+// some notes on the behavior below:
+// because the 'stack' member is a one shot access variable (the raw stack is
+// formatted on accessing it)
+// we try to avoid modifying what the user would have wanted
+// thus we use the previous value for prepareStackTrace
+//
+// The reason we store the callsite variable is because prepareStackTrace
+// will not be called again once it has been called for a given error object
+// but we want to support getting the stack out of the error multiple times (cause why not)
 module.exports = function(err) {
 
     // save original stacktrace
     var save = Error.prepareStackTrace;
 
     // replace capture with our function
-    Error.prepareStackTrace = capture;
+    Error.prepareStackTrace =  function(err, trace) {
 
-    var captured = err.stack;
+        // cache stack frames so we don't have to get them again
+        // use a non-enumerable property
+        Object.defineProperty(err, '_sb_callsites', {
+            value: trace
+        });
 
-    // return original capture
+        return (save || FormatStackTrace)(err, trace);
+    };
+
+    // force capture of the stack frames
+    err.stack;
+
+    // someone already asked for the stack so we can't do this trick
+    // TODO fallback to string parsing?
+    if (!err._sb_callsites) {
+        return [];
+    }
+
+    // return original capture function
     Error.prepareStackTrace = save;
 
-    return captured;
+    return err._sb_callsites;
 };
+
